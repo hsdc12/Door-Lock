4e10516a06009093b41e10fd03f70d884256d91f
hsdc12 <jeremyching800@gmail.com>
2025-07-29 22:26:05 +0100

    Added further setup subroutine for door lock to clean up code.

--- Door_Lock.ino
+++ Door_Lock.ino
@@ -1,12 +1,7 @@
 #include "main.h"  // Or: #include "src/Lock/Lock.h" if you want to skip main.h
 
 void setup() {
-  pinMode(2, OUTPUT);  // Piezo
-  pinMode(4, OUTPUT);  // Solenoid
-  pinMode(5, OUTPUT);  // green LED
-  pinMode(6, OUTPUT);  // red LED
-
-  lockdoor();          // Start with the door locked
+  setupLock();  // Initialize the lock system
 }
 
 void loop() {
